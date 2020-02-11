367ac19a43a3a2aaff5de9ff73e1ea12c002121a
Ghost-EXE <Ghost-EXE@users.noreply.github.com>
2020-02-11 11:24:38 +0200

    Game Time and scoreBoard

--- src/main/java/kehtnakhkta18/App.java
+++ src/main/java/kehtnakhkta18/App.java
@@ -3,7 +3,12 @@ package kehtnakhkta18;
 
 import java.sql.Time;
 import java.util.concurrent.TimeUnit;
-
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.IOException;
+import java.io.BufferedWriter;
+import java.io.FileOutputStream;
+import java.io.OutputStreamWriter;
 
 /**
  *
@@ -27,6 +32,7 @@ public final class App {
 
         MMGame.initializeForm();
         time();
+
     }
 
 
@@ -36,7 +42,8 @@ public final class App {
         long gameMinutes = 0;
         int x = 1;
         int y = 0;
-        while (true) { //siia panna while gameEnd = false või midagi selist
+        String gametime;
+        while (gameSeconds < 10) { //siia panna while gameEnd = false või midagi selist
 
             long end = System.nanoTime();
 
@@ -54,6 +61,68 @@ public final class App {
             System.out.println(gameMinutes + ":" + (gameSeconds - 60 * y));// labelise panna
 
         }
+        gametime = gameMinutes + ":" + gameSeconds;
+        scoreBoard(gametime);
+    }
+
+    public static void scoreBoard(String gametime) {
+
+        String username = null; //Ajutine osa, kuni saaks mingid moodi ühendada teiste osadega
+        String skoor = null;
+        String time = gametime;
+        String turns = null;
+
+        try {
+            FileReader reader = new FileReader("score.csv");
+            BufferedReader bufferedReader = new BufferedReader(reader);
+
+            String line;
+
+            while ((line = bufferedReader.readLine()) != null) {
+                System.out.println(line); //Et näha mis on score.csv failis, ilma et peaks seda lahti teha
+            }
+            reader.close();
+
+            // Üleval, BufferedReader, all BufferedWriter
+
+            FileOutputStream outputStream = new FileOutputStream("score.csv", true);
+            OutputStreamWriter outputStreamWriter = new OutputStreamWriter(outputStream, "UTF-16");
+            BufferedWriter bufferedWriter = new BufferedWriter(outputStreamWriter);
+
+            bufferedWriter.write("------------------------");
+            bufferedWriter.newLine();
+            if (username != null) {
+                bufferedWriter.write("Nimi: " + username);
+            } else {
+                bufferedWriter.write("Kasutajanimi pole leitud!");
+            }
+            bufferedWriter.newLine();
+            if (turns != null) {
+                bufferedWriter.write("Käike: " + turns);
+            } else {
+                bufferedWriter.write("Miski läks käikude lugemisel valesti!");
+            }
+            bufferedWriter.newLine();
+            if (time != null) {
+                bufferedWriter.write("Aeg: " + time);
+            } else {
+                bufferedWriter.write("Miski läks aja lugemisel valesti!");
+            }
+            bufferedWriter.newLine();
+            if (skoor != null) {
+                bufferedWriter.write("Skoor: " + skoor);
+            } else {
+                bufferedWriter.write("Miski läks skoori lugemisel valesti!");
+            }
+            bufferedWriter.newLine();
+            bufferedWriter.write("------------------------");
+            bufferedWriter.newLine();
+
+            bufferedWriter.close();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
     }
 
 
